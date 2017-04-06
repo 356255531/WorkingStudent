05cb8441a1801c87ccc9feff8540c67b682ec13e
Zhiwei Han <hanzw356255531@icloud.com>
2017-04-06 21:53:20 +0200

    new sobel shader

--- Project/sobelfragshader.glsl
+++ Project/sobelfragshader.glsl
@@ -3,21 +3,21 @@ uniform sampler2D tex;
 uniform vec2 texelsize;
 void main(void) 
 {
-	vec3 tm1m1 = texture2D(tex,tcoord+vec2(-1,-1)*texelsize);
-	vec3 tm10 = texture2D(tex,tcoord+vec2(-1,0)*texelsize);
-	vec3 tm1p1 = texture2D(tex,tcoord+vec2(-1,1)*texelsize);
-	vec3 tp1m1 = texture2D(tex,tcoord+vec2(1,-1)*texelsize);
-	vec3 tp10 = texture2D(tex,tcoord+vec2(1,0)*texelsize);
-	vec3 tp1p1 = texture2D(tex,tcoord+vec2(1,1)*texelsize);
-	vec3 t0m1 = texture2D(tex,tcoord+vec2(0,-1)*texelsize);
-	vec3 t0p1 = texture2D(tex,tcoord+vec2(0,-1)*texelsize);
+    vec4 tm1m1 = texture2D(tex,tcoord + vec2(-1, -1)* texelsize);
+    vec4 tm10 = texture2D(tex,tcoord + vec2(-1, 0)* texelsize);
+    vec4 tm1p1 = texture2D(tex,tcoord + vec2(-1, 1)* texelsize);
+    vec4 tp1m1 = texture2D(tex,tcoord + vec2(1, -1)* texelsize);
+    vec4 tp10 = texture2D(tex,tcoord + vec2(1, 0)* texelsize);
+    vec4 tp1p1 = texture2D(tex,tcoord + vec2(1, 1)* texelsize);
+    vec4 t0m1 = texture2D(tex,tcoord + vec2(0, -1)* texelsize);
+    vec4 t0p1 = texture2D(tex,tcoord + vec2(0, -1)* texelsize);
 
-	vec3 x_gradient_3d = -1.0*tm1m1 + -2.0*tm10 + -1.0*tm1p1 + 1.0*tp1m1 + 2.0*tp10 + 1.0*tp1p1;
-	vec3 y_gradient_3d = -1.0*tm1m1 + -2.0*t0m1 + -1.0*tp1m1 + 1.0*tm1p1 + 2.0*t0p1 + 1.0*tp1p1;
+    vec4 x_gradient_3d = -1.0*tm1m1 + -2.0*tm10 + -1.0*tm1p1 + 1.0*tp1m1 + 2.0*tp10 + 1.0*tp1p1;
+    vec4 y_gradient_3d = -1.0*tm1m1 + -2.0*t0m1 + -1.0*tp1m1 + 1.0*tm1p1 + 2.0*t0p1 + 1.0*tp1p1;
 
-	float x_gradient = (x_gradient_3d.x + x_gradient_3d.y + x_gradient_3d.z) / 3.0;
-	float y_gradient = (y_gradient_3d.x + y_gradient_3d.y + y_gradient_3d.z) / 3.0;
-	float gradient_product = x_gradient * y_gradient;
+    float x_gradient = 0.21 * x_gradient_3d.x + 0.72 * x_gradient_3d.y + 0.07 * x_gradient_3d.z;
+    float y_gradient = 0.21 * y_gradient_3d.x + 0.72 * y_gradient_3d.y + 0.07 * y_gradient_3d.z;
+    float gradient_product = x_gradient * y_gradient;
 
-	gl_FragColor = vec3(x_gradient, y_gradient, gradient_product);
+    gl_FragColor = vec4(x_gradient, y_gradient, gradient_product, 1.0);
 }
