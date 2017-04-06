512d9d8b090078523912ee7646a216025ddaf8bd
Zhiwei Han <hanzw356255531@icloud.com>
2017-04-06 21:18:56 +0200

    simplify picam

--- Project/picam.cpp
+++ Project/picam.cpp
@@ -36,78 +36,7 @@ int main(int argc, const char **argv)
 	harris_response_tex.GenerateFrameBuffer();
 	select_tex.CreateRGBA(MAIN_TEXTURE_WIDTH, MAIN_TEXTURE_HEIGHT);
 	select_tex.GenerateFrameBuffer();
-	// //create 4 textures of decreasing size
-	// GfxTexture   ytexture,utexture,vtexture,
-	//          rgbtextures[32], blurtexture,greysobeltexture,
-	//          sobeltexture, mediantexture,redtexture,
-	//          dilatetexture, erodetexture,threshtexture;
-	// ytexture.CreateGreyScale(MAIN_TEXTURE_WIDTH,MAIN_TEXTURE_HEIGHT);
-	// utexture.CreateGreyScale(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// vtexture.CreateGreyScale(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
 
-	// GfxTexture yreadtexture,ureadtexture,vreadtexture;
-	// yreadtexture.CreateRGBA(MAIN_TEXTURE_WIDTH,MAIN_TEXTURE_HEIGHT);
-	// yreadtexture.GenerateFrameBuffer();
-	// ureadtexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// ureadtexture.GenerateFrameBuffer();
-	// vreadtexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// vreadtexture.GenerateFrameBuffer();
-
-	// GfxTexture* texture_grid[TEXTURE_GRID_COLS*TEXTURE_GRID_ROWS];
-	// memset(texture_grid,0,sizeof(texture_grid));
-	// int next_texture_grid_entry = 0;
-
-	// texture_grid[next_texture_grid_entry++] = &ytexture;
-	// texture_grid[next_texture_grid_entry++] = &utexture;
-	// texture_grid[next_texture_grid_entry++] = &vtexture;
-
-	// int levels=1;
-	// while( (MAIN_TEXTURE_WIDTH>>levels)>16 && (MAIN_TEXTURE_HEIGHT>>levels)>16 && (levels+1)<32)
-	//  levels++;
-	// printf("Levels used: %d, smallest level w/h: %d/%d\n",levels,MAIN_TEXTURE_WIDTH>>(levels-1),MAIN_TEXTURE_HEIGHT>>(levels-1));
-
-	// for(int i = 0; i < levels; i++)
-	// {
-	//  rgbtextures[i].CreateRGBA(MAIN_TEXTURE_WIDTH>>i,MAIN_TEXTURE_HEIGHT>>i);
-	//  rgbtextures[i].GenerateFrameBuffer();
-	//  texture_grid[next_texture_grid_entry++] = &rgbtextures[i];
-	// }
-
-	// blurtexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// blurtexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &blurtexture;
-
-	// sobeltexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// sobeltexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &sobeltexture;
-
-	// greysobeltexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// greysobeltexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &greysobeltexture;
-
-	// mediantexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// mediantexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &mediantexture;
-
-	// redtexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// redtexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &redtexture;
-
-	// dilatetexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// dilatetexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &dilatetexture;
-
-	// erodetexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// erodetexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &erodetexture;
-
-	// threshtexture.CreateRGBA(MAIN_TEXTURE_WIDTH/2,MAIN_TEXTURE_HEIGHT/2);
-	// threshtexture.GenerateFrameBuffer();
-	// texture_grid[next_texture_grid_entry++] = &threshtexture;
-
-	// float texture_grid_col_size = 2.f/TEXTURE_GRID_COLS;
-	// float texture_grid_row_size = 2.f/TEXTURE_GRID_ROWS;
-	// int selected_texture = -1;
 
 	printf("Running frame loop\n");
 
@@ -118,7 +47,6 @@ int main(int argc, const char **argv)
 	clock_gettime(CLOCK_REALTIME, &gettime_now);
 	start_time = gettime_now.tv_nsec ;
 	double total_time_s = 0;
-	// bool do_pipeline = false;
 
 	initscr();      /* initialize the curses library */
 	keypad(stdscr, TRUE);  /* enable keyboard mapping */
@@ -129,36 +57,6 @@ int main(int argc, const char **argv)
 
 	for (int i = 0; i < 3000; i++)
 	{
-		// int ch = getch();
-		// if(ch != ERR)
-		// {
-		//  if(ch == 'q')
-		//      break;
-		//  else if(ch == 'a')
-		//      selected_texture = (selected_texture+1)%(TEXTURE_GRID_ROWS*TEXTURE_GRID_ROWS);
-		//  else if(ch == 's')
-		//      selected_texture = -1;
-		//  else if(ch == 'd')
-		//      do_pipeline = !do_pipeline;
-		//  else if(ch == 'w')
-		//  {
-		//      SaveFrameBuffer("tex_fb.png");
-		//      yreadtexture.Save("tex_y.png");
-		//      ureadtexture.Save("tex_u.png");
-		//      vreadtexture.Save("tex_v.png");
-		//      rgbtextures[0].Save("tex_rgb_hi.png");
-		//      rgbtextures[levels-1].Save("tex_rgb_low.png");
-		//      blurtexture.Save("tex_blur.png");
-		//      sobeltexture.Save("tex_sobel.png");
-		//      dilatetexture.Save("tex_dilate.png");
-		//      erodetexture.Save("tex_erode.png");
-		//      threshtexture.Save("tex_thresh.png");
-		//  }
-
-		//  move(0,0);
-		//  refresh();
-		// }
-
 		//spin until we have a camera frame
 		const void* frame_data; int frame_sz;
 		while (!cam->BeginReadFrame(frame_data, frame_sz)) {};
@@ -186,72 +84,6 @@ int main(int argc, const char **argv)
 		DrawSobelRect(&y_tex, -1, -1, 1, 1, &sobel_tex);
 		// DrawBlurredRect(&sobel_tex, -1,-1,1,1,&blurred_sobel_tex);
 		DrawTextureRect(&sobel_tex, -1, -1, 1, 1, NULL);
-		// float aspect_ratio = float(MAIN_TEXTURE_WIDTH)/float(MAIN_TEXTURE_HEIGHT);
-		// float screen_aspect_ratio = 1280.f/720.f;
-		// for(int texidx = 0; texidx<levels; texidx++)
-		//  DrawYUVTextureRect(&ytexture,&utexture,&vtexture,-1.f,-1.f,1.f,1.f,&rgbtextures[texidx]);
-
-		// //these are just here so we can access the yuv data cpu side - opengles doesn't let you read grey ones cos they can't be frame buffers!
-		// // DrawTextureRect(&ytexture,-1,-1,1,1,&yreadtexture);
-		// // DrawTextureRect(&utexture,-1,-1,1,1,&ureadtexture);
-		// // DrawTextureRect(&vtexture,-1,-1,1,1,&vreadtexture);
-
-		// if(!do_pipeline)
-		// {
-		//  // if(selected_texture == -1 || &blurtexture == texture_grid[selected_texture])
-		//  //  DrawBlurredRect(&rgbtextures[1],-1.f,-1.f,1.f,1.f,&blurtexture);
-
-		//  // if(selected_texture == -1 || &sobeltexture == texture_grid[selected_texture])
-		//  //  DrawSobelRect(&rgbtextures[1],-1.f,-1.f,1.f,1.f,&sobeltexture);
-
-		//  if(selected_texture == -1 || &greysobeltexture == texture_grid[selected_texture])
-		//      DrawSobelRect(&ytexture,-1.f,-1.f,1.f,1.f,&greysobeltexture);
-
-		//  // if(selected_texture == -1 || &mediantexture == texture_grid[selected_texture])
-		//  //  DrawMedianRect(&rgbtextures[1],-1.f,-1.f,1.f,1.f,&mediantexture);
-
-		//  // if(selected_texture == -1 || &redtexture == texture_grid[selected_texture])
-		//  //  DrawMultRect(&rgbtextures[1],-1.f,-1.f,1.f,1.f,1,0,0,&redtexture);
-
-		//  // if(selected_texture == -1 || &dilatetexture == texture_grid[selected_texture])
-		//  //  DrawDilateRect(&rgbtextures[1],-1.f,-1.f,1.f,1.f,&dilatetexture);
-
-		//  // if(selected_texture == -1 || &erodetexture == texture_grid[selected_texture])
-		//  //  DrawErodeRect(&rgbtextures[1],-1.f,-1.f,1.f,1.f,&erodetexture);
-
-		//  // if(selected_texture == -1 || &threshtexture == texture_grid[selected_texture])
-		//  //  DrawThreshRect(&ytexture,-1.f,-1.f,1.f,1.f,0.25f,0.25f,0.25f,&threshtexture);
-
-		//  if(selected_texture == -1)
-		//  {
-		//      for(int col = 0; col < TEXTURE_GRID_COLS; col++)
-		//      {
-		//          for(int row = 0; row < TEXTURE_GRID_ROWS; row++)
-		//          {
-		//              if(GfxTexture* tex = texture_grid[col+row*TEXTURE_GRID_COLS])
-		//              {
-		//                  float colx = -1.f+col*texture_grid_col_size;
-		//                  float rowy = -1.f+row*texture_grid_row_size;
-		//                  DrawTextureRect(tex,colx,rowy,colx+texture_grid_col_size,rowy+texture_grid_row_size,NULL);
-		//              }
-		//          }
-		//      }
-		//  }
-		//  else
-		//  {
-		//      if(GfxTexture* tex = texture_grid[selected_texture])
-		//          DrawTextureRect(tex,-1,-1,1,1,NULL);
-		//  }
-		// }
-		// else
-		// {
-		//  DrawMedianRect(&ytexture,-1.f,-1.f,1.f,1.f,&mediantexture);
-		//  DrawSobelRect(&mediantexture,-1.f,-1.f,1.f,1.f,&sobeltexture);
-		//  DrawErodeRect(&sobeltexture,-1.f,-1.f,1.f,1.f,&erodetexture);
-		//  // DrawDilateRect(&erodetexture,-1.f,-1.f,1.f,1.f,&dilatetexture);
-		//  DrawThreshRect(&erodetexture,-1.f,-1.f,1.f,1.f,0.05f,0.05f,0.05f,&threshtexture);
-		//  DrawTextureRect(&threshtexture,-1,-1,1,1,NULL);
-		// }
 
 		EndFrame();
 
