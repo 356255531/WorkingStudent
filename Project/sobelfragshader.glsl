142cea61e4298327cf808f223bc9420526a5f3b4
Zhiwei Han <hanzw356255531@icloud.com>
2017-05-03 00:19:08 +0200

    new sobel pass debug

--- Project/sobelfragshader.glsl
+++ Project/sobelfragshader.glsl
@@ -1,22 +1,39 @@
+
 varying vec2 tcoord;
 uniform sampler2D tex;
 uniform vec2 texelsize;
 void main(void) 
 {
-    vec4 tm1m1 = texture2D(tex,tcoord + vec2(-1, -1)* texelsize);
-    vec4 tm10 = texture2D(tex,tcoord + vec2(-1, 0)* texelsize);
-    vec4 tm1p1 = texture2D(tex,tcoord + vec2(-1, 1)* texelsize);
-    vec4 tp1m1 = texture2D(tex,tcoord + vec2(1, -1)* texelsize);
-    vec4 tp10 = texture2D(tex,tcoord + vec2(1, 0)* texelsize);
-    vec4 tp1p1 = texture2D(tex,tcoord + vec2(1, 1)* texelsize);
-    vec4 t0m1 = texture2D(tex,tcoord + vec2(0, -1)* texelsize);
-    vec4 t0p1 = texture2D(tex,tcoord + vec2(0, -1)* texelsize);
+    vec2 width_step = vec2(texelsize.r, 0.0);
+    vec2 height_step = vec2(0.0, texelsize.g);
 
-    vec4 x_gradient_3d = -1.0*tm1m1 + -2.0*tm10 + -1.0*tm1p1 + 1.0*tp1m1 + 2.0*tp10 + 1.0*tp1p1;
-    vec4 y_gradient_3d = -1.0*tm1m1 + -2.0*t0m1 + -1.0*tp1m1 + 1.0*tm1p1 + 2.0*t0p1 + 1.0*tp1p1;
+    vec4 bottom_left_color =    texture2D(  tex,
+                                            tcoord + height_step - width_step);
+    vec4 bottom_color =         texture2D(  tex,
+                                            tcoord + height_step);
+    vec4 bottom_right_color =   texture2D(  tex,
+                                            tcoord + height_step + width_step);
+    vec4 top_left_color =       texture2D(  tex,
+                                            tcoord - height_step - width_step);
+    vec4 top_color =            texture2D(  tex,
+                                            tcoord - height_step);
+    vec4 top_right_color =      texture2D(  tex,
+                                            tcoord - height_step + width_step);
+    vec4 left_color =           texture2D(  tex,
+                                            tcoord - width_step);
+    vec4 right_color =          texture2D(  tex,
+                                            tcoord + width_step);
 
-    float x_gradient = 0.21 * x_gradient_3d.x + 0.72 * x_gradient_3d.y + 0.07 * x_gradient_3d.z;
-    float y_gradient = 0.21 * y_gradient_3d.x + 0.72 * y_gradient_3d.y + 0.07 * y_gradient_3d.z;
+    vec4 x_gradient_3d =    -1.0 * bottom_left_color + -2.0 * left_color + -1.0 * top_left_color +
+                             1.0 * bottom_right_color + 2.0 * right_color + 1.0 * top_right_color;
+    vec4 y_gradient_3d =    -1.0 * bottom_left_color + -2.0 * bottom_color + -1.0 * bottom_right_color + 
+                            1.0 * top_left_color + 2.0 * top_color + 1.0 * top_right_color;
 
-    gl_FragColor = vec4(x_gradient, y_gradient, 0.0, 1.0);
+    float x_gradient = 0.21 * x_gradient_3d.r + 0.72 * x_gradient_3d.g + 0.07 * x_gradient_3d.b;
+    float y_gradient = 0.21 * y_gradient_3d.r + 0.72 * y_gradient_3d.g + 0.07 * y_gradient_3d.b;
+
+    gl_FragColor = vec4(    x_gradient, 
+                            y_gradient,     
+                            x_gradient * y_gradient, 
+                            1.0);
 }
